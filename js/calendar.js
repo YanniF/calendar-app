4d6f7c6693b3045d53ef416de1c05d91b687aa3e
Yanni Fraga <yanni_fernanda@hotmail.com>
2019-01-13 16:45:34 -0200

    Generating calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,22 +1,96 @@
+"use strict";
 
+/*
+  TODO:
+  - modal: add animation 
+           add event on cancel button
+           close modal when clicking outside of it
+           add date on title
 
-function init() {
-  document.querySelector(".calendar").addEventListener("click", e => {
-    // this way it'll work on chrome and firefox
-    const btnAdd = e.target.classList.contains('.add') ? e.target : e.target.closest('.add');
+*/
 
-    if(btnAdd) {
-      document.querySelector(".modal-wrapper").style.display = "flex";
+const UICtrl = (function() {
+  return {
+    showModal: (e) => {
+      e = e.target;
+
+      // this will solve the problem on firefox
+      if(e.classList.contains('.add') || e.closest('.add')) {
+        document.querySelector(".add-modal").style.display = "flex";
+      }
+      else if(e.classList.contains('.edit') || e.closest('.edit')) {
+        document.querySelector(".add-modal").style.display = "flex";
+      }
+      else if(e.classList.contains('.del') || e.closest('.del')) {
+        document.querySelector(".del-modal").style.display = "flex";
+      }
+    },
+
+    hideModal: (e) => {
+      const btnClose = e.target.classList.contains('.times') ? e.target : e.target.closest('.times');
+      
+      if(btnClose) {
+        e.target.parentElement.parentElement.parentElement.style.display = "none";
+      }
+    },
+    test: () => {
+      console.log("test")
     }
-  });
+  }
+})();
 
-  document.querySelector(".modal-wrapper").addEventListener("click", e => {
-    const btnClose = e.target.classList.contains('.times') ? e.target : e.target.closest('.times');
+const app = ((ui) => {
 
-    if(btnClose) {
-      document.querySelector(".modal-wrapper").style.display = "none";
+  const addListeners = function () {
+    document.querySelector(".calendar").addEventListener("click", ui.showModal);
+    document.querySelectorAll(".modal-wrapper").forEach(modal => modal.addEventListener("click", ui.hideModal));
+  }
+
+  return {
+    init: () => {
+      addListeners();
     }
-  });
+  }
+})(UICtrl);
+
+app.init();
+
+let today = new Date();
+let nDays = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate(); // number of days in this month
+let firstDayWeek = new Date(today.getFullYear(), today.getMonth(), 1).getDay();// day of the week
+let table = "";
+let td = "";
+
+table += "<tr>";
+
+// start on the right day of the week
+for(let c = 0; c < firstDayWeek; c++) {
+  table += "<td></td>";
 }
 
-init();
+for(let c = 1; c <= nDays; c++) {  
+  td = `
+  <td>
+    <span class="table-day">${c}</span>
+    ${c >= today.getDate() ? '<a href="#" class="btn add"><span class="fas fa-plus"></span></a>' : ''}
+  </td>`;
+ 
+  // last element
+  if(c === nDays) {
+    table += td;
+  }
+  else if((c + firstDayWeek) % 7 === 0) { // new line
+    table += `
+      ${td} 
+    </tr> 
+    <tr> `;
+  }
+  else {
+    table += td;
+  }
+  
+}
+
+table += "</tr>";
+
+document.querySelector('.calendar-body').insertAdjacentHTML("beforeend", table)
